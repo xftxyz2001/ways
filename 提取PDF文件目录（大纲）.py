dec947782401447dad147d10c23d2021af0362d2
通晓宇宙 <2581011320@qq.com>
2022-10-12 10:59:46 +0800

    pdf文件目录提取

---
+++ 提取PDF文件目录（大纲）.py
@@ -0,0 +1,29 @@
+from PyPDF2 import PdfFileReader as pdf_read
+
+# 每个书签的索引格式
+#{'/Title': '书签名', '/Page': '指向的目标页数', '/Type': '类型'}
+
+filename = '1.pdf'  # 要提取目录的pdf文件名
+
+
+directory_str = ''
+
+
+def bookmark_listhandler(list):
+    global directory_str
+    for message in list:
+        if isinstance(message, dict):
+            directory_str += message['/Title'] + '\n'
+            # print(message['/Title'])
+        else:
+            bookmark_listhandler(message)
+
+
+with open(filename, 'rb') as f:
+    pdf = pdf_read(f)
+    # 检索文档中存在的文本大纲,返回的对象是一个嵌套的列表
+    text_outline_list = pdf.getOutlines()
+    bookmark_listhandler(text_outline_list)
+
+with open('directory.txt', 'w', encoding='utf-8') as f:
+    f.write(directory_str)
