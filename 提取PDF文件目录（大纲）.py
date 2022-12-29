406746f629890c8915022c00cd27f9aef50660f4
通晓宇宙 <2581011320@qq.com>
2022-12-29 12:02:46 +0800

    pdf新版

--- 提取PDF文件目录（大纲）.py
+++ 提取PDF文件目录（大纲）.py
@@ -1,4 +1,4 @@
-from PyPDF2 import PdfFileReader as pdf_read
+from PyPDF2 import PdfReader as pdf_read
 
 # 每个书签的索引格式
 #{'/Title': '书签名', '/Page': '指向的目标页数', '/Type': '类型'}
@@ -22,7 +22,7 @@ def bookmark_listhandler(list):
 with open(filename, 'rb') as f:
     pdf = pdf_read(f)
     # 检索文档中存在的文本大纲,返回的对象是一个嵌套的列表
-    text_outline_list = pdf.getOutlines()
+    text_outline_list = pdf.outline
     bookmark_listhandler(text_outline_list)
 
 with open('directory.txt', 'w', encoding='utf-8') as f:
