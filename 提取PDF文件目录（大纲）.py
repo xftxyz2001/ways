7c5718dc5cad7d275d0e7aad032c1c65ad64168e
通晓宇宙 <2581011320@qq.com>
2023-04-23 07:22:11 +0800

    pdf提取大纲

--- 提取PDF文件目录（大纲）.py
+++ 提取PDF文件目录（大纲）.py
@@ -1,29 +1,27 @@
-from PyPDF2 import PdfReader as pdf_read
+from PyPDF2 import PdfReader
 
 # 每个书签的索引格式
-#{'/Title': '书签名', '/Page': '指向的目标页数', '/Type': '类型'}
+# {'/Title': '书签名', '/Page': '指向的目标页数', '/Type': '类型'}
 
-filename = '1.pdf'  # 要提取目录的pdf文件名
-
-
+filename = '1'  # 要提取目录的pdf文件名（不包含后缀）
 directory_str = ''
 
 
-def bookmark_listhandler(list):
+def bookmark_listhandler(list, level=1):
     global directory_str
     for message in list:
         if isinstance(message, dict):
-            directory_str += message['/Title'] + '\n'
-            # print(message['/Title'])
+            directory_str = directory_str + '#' * \
+                level + message['/Title'] + '\n'
         else:
-            bookmark_listhandler(message)
+            bookmark_listhandler(message, level+1)
 
 
-with open(filename, 'rb') as f:
-    pdf = pdf_read(f)
+with open(filename + '.pdf', 'rb') as f:
+    pdf = PdfReader(f)
     # 检索文档中存在的文本大纲,返回的对象是一个嵌套的列表
     text_outline_list = pdf.outline
     bookmark_listhandler(text_outline_list)
 
-with open('directory.txt', 'w', encoding='utf-8') as f:
+with open(filename + '.md', 'w', encoding='utf-8') as f:
     f.write(directory_str)
