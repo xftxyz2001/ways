0f3aa7d1948a7418e01aec49fbd4dc2704252131
通晓宇宙 <2581011320@qq.com>
2023-06-08 01:23:09 +0800

    命令提示

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+New-Alias -Name git -Value "C:\Program Files\Git\bin\git.exe"
+Import-Module posh-git
+
+# Shows navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Autocompletion for arrow keys
+Set-PSReadLineKeyHandler -Key UpArrow -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchBackward()
+    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+}
+Set-PSReadLineKeyHandler -Key DownArrow -ScriptBlock {
+    [Microsoft.PowerShell.PSConsoleReadLine]::HistorySearchForward()
+    [Microsoft.PowerShell.PSConsoleReadLine]::EndOfLine()
+}
+
+# auto suggestions
+Import-Module PSReadLine
+Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+Set-PSReadLineOption -PredictionViewStyle ListView
