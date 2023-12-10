16b66fbeb5b4c858ac895aef314ebf87c7abe7bc
通晓宇宙 <2581011320@qq.com>
2023-12-10 14:00:58 +0800

    QPushButton右键单击

---
+++ RightClickButton.py
@@ -0,0 +1,27 @@
+from PyQt6.QtWidgets import QPushButton
+from PyQt6.QtCore import pyqtSignal, Qt
+
+
+class RightClickButton(QPushButton):
+    rightClicked = pyqtSignal()
+
+    def __init__(self, text, parent=None):
+        super().__init__(text, parent)
+
+    def mousePressEvent(self, event):
+        if event.buttons() == Qt.MouseButton.LeftButton:
+            self.clicked.emit()
+        elif event.buttons() == Qt.MouseButton.RightButton:
+            self.rightClicked.emit()
+
+
+if __name__ == "__main__":
+    from PyQt6.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    window = RightClickButton("右键点击我")
+    window.clicked.connect(lambda: print("响应左键点击"))
+    window.rightClicked.connect(lambda: print("响应右键点击"))
+    window.show()
+    sys.exit(app.exec())
